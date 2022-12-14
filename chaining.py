f0124b70f220d7ed0874e7bd1113923c7747d42e
Cicero Cordeiro <cicero.cordeiro1995@gmail.com>
2022-12-14 15:51:16 -0300

    Create chaining.py

---
+++ chaining.py
@@ -0,0 +1,10 @@
+tamanho_cabelo = 51
+
+if tamanho_cabelo <= 20:
+    print('Pague R$50,00')
+elif 21 <= tamanho_cabelo <= 30:
+    print('Pague R$70,00')
+elif 31 <= tamanho_cabelo <= 50:
+    print('Pague R$100,00')
+else:
+    print('Consulta na recepção')
